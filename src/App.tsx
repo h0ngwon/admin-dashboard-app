7c0fa1920aeb5c908f9e55c8b033d712d11d63a9
h0ngwon <w___on@naver.com>
2024-11-27 23:02:39 +0900

    feat: 헤더 및 현재 사용자 기능

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import {
-	ErrorComponent,
-	ThemedLayoutV2,
-	ThemedSiderV2,
-	useNotificationProvider,
-} from "@refinedev/antd";
+import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import routerBindings, {
@@ -18,19 +13,10 @@ import routerBindings, {
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import {
-	BlogPostCreate,
-	BlogPostEdit,
-	BlogPostList,
-	BlogPostShow,
-} from "./pages/blog-posts";
-import {
-	CategoryCreate,
-	CategoryEdit,
-	CategoryList,
-	CategoryShow,
-} from "./pages/categories";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
 import { ForgotPassword } from "./pages/forgotPassword";
+import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { authProvider, dataProvider, liveProvider } from "./providers";
@@ -47,28 +33,7 @@ function App() {
 							notificationProvider={useNotificationProvider}
 							routerProvider={routerBindings}
 							authProvider={authProvider}
-							resources={[
-								{
-									name: "blog_posts",
-									list: "/blog-posts",
-									create: "/blog-posts/create",
-									edit: "/blog-posts/edit/:id",
-									show: "/blog-posts/show/:id",
-									meta: {
-										canDelete: true,
-									},
-								},
-								{
-									name: "categories",
-									list: "/categories",
-									create: "/categories/create",
-									edit: "/categories/edit/:id",
-									show: "/categories/show/:id",
-									meta: {
-										canDelete: true,
-									},
-								},
-							]}
+							resources={resources}
 							options={{
 								syncWithLocation: true,
 								warnWhenUnsavedChanges: true,
@@ -86,66 +51,13 @@ function App() {
 												<CatchAllNavigate to="/login" />
 											}
 										>
-											<ThemedLayoutV2
-												// Header={Header}
-												Sider={(props) => (
-													<ThemedSiderV2
-														{...props}
-														fixed
-													/>
-												)}
-											>
+											<Layout>
 												<Outlet />
-											</ThemedLayoutV2>
+											</Layout>
 										</Authenticated>
 									}
 								>
-									<Route
-										index
-										element={
-											<NavigateToResource resource="blog_posts" />
-										}
-									/>
-									<Route path="/blog-posts">
-										<Route
-											index
-											element={<BlogPostList />}
-										/>
-										<Route
-											path="create"
-											element={<BlogPostCreate />}
-										/>
-										<Route
-											path="edit/:id"
-											element={<BlogPostEdit />}
-										/>
-										<Route
-											path="show/:id"
-											element={<BlogPostShow />}
-										/>
-									</Route>
-									<Route path="/categories">
-										<Route
-											index
-											element={<CategoryList />}
-										/>
-										<Route
-											path="create"
-											element={<CategoryCreate />}
-										/>
-										<Route
-											path="edit/:id"
-											element={<CategoryEdit />}
-										/>
-										<Route
-											path="show/:id"
-											element={<CategoryShow />}
-										/>
-									</Route>
-									<Route
-										path="*"
-										element={<ErrorComponent />}
-									/>
+									<Route index element={<Home />} />
 								</Route>
 								<Route
 									element={
